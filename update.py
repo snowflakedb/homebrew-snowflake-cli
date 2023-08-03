d78c63f180bc61e69829889f0b50ce31ff7dab7c
Tomasz Urbaszek <tomasz.urbaszek@snowflake.com>
2023-08-03 19:29:43 +0200

    Update v1.0.1 formula (#6)

--- update.py
+++ update.py
@@ -11,22 +11,21 @@ def main():
         loader=jinja2.loaders.FileSystemLoader(Path(__file__).parent)
     )
     template = env.get_template("snowcli.tmpl.rb")
-    result = subprocess.check_output(["poet", "snowflake-cli-labs"], encoding="utf-8")
+    packages = subprocess.check_output(["poet", "snowflake-cli-labs"], encoding="utf-8")
 
     sf_pattern = r'\s+resource \"snowflake-cli-labs\" do\s+url \"(.+)\"\s+sha256 \"(\w+)\"\s+end\n'
-    match = re.findall(sf_pattern, result)
+    match = re.findall(sf_pattern, packages)
     if not match:
         raise ValueError("snowflake dependency not present in deps")
     sf_url, sf_sha = match[0]
 
-    packages = re.sub(sf_pattern, "\n", result)
-    packages = indent(packages, "  ")
+
 
     with open("Formula/snowcli.rb", "w+") as fh:
         fh.write(template.render(
             sf_url=sf_url,
             sf_sha=sf_sha,
-            packages=packages
+            packages=indent(packages, "  ")
         ))
 
 
