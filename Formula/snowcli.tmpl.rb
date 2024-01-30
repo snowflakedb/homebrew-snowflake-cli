395004d58717fdddfb436771d717e4b3346b2234
Patryk Czajka <patryk.czajka@snowflake.com>
2024-01-30 13:47:22 +0100

    Faster formula for snowcli installation (#14)
    * Faster formula for snowcli installation
    
    * Add workflow to test installation

--- Formula/snowcli.tmpl.rb
+++ Formula/snowcli.tmpl.rb
@@ -5,11 +5,7 @@
     url "{{ sf_url }}"
     sha256 "{{ sf_sha }}"
 
-    depends_on "rust" => :build
     depends_on "python3"
-
-{{ packages }}
-
 
     def install
       ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
@@ -17,23 +13,9 @@
       venv = virtualenv_create(libexec, "python3", system_site_packages: false, without_pip: false)
       venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
         "-m", "pip", "install", "pip==22.3.1"
-      resources.each do |r|
-        if r.name == "snowflake-connector-python" or r.name == "snowflake-connector-python-nightly"
-          # workaround for installing `snowflake-connector-python`
-          # package w/o build-system deps (e.g. pyarrow)
-          # adds the `--no-use-pep517` parameter
-          # learned from dbt-homebrew
-          r.stage do
-            venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip", "install",
-              "-v", "--no-deps", "--no-binary", ":all:", "--ignore-installed", "--no-use-pep517", Pathname.pwd
-          end
-        else
-          venv.pip_install r
-        end
-      end
-    venv.pip_install_and_link buildpath
-
-    bin.install_symlink "#{libexec}/bin/snow" => "snow"
+      venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip",
+        "install", "snowflake-cli-labs=={{ version }}"
+      bin.install_symlink "#{libexec}/bin/snow" => "snow"
     end
 
     test do
