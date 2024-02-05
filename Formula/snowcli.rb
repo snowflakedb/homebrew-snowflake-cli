7522f2d55560f7a1249c2fe2f5dcd45d34f0a86a
Patryk Czajka <patryk.czajka@snowflake.com>
2024-02-05 14:26:07 +0100

    Update formula to v2.0.0

--- Formula/snowcli.rb
+++ Formula/snowcli.rb
@@ -2,8 +2,8 @@
     include Language::Python::Virtualenv
     desc "A CLI for Snowflake development"
     homepage "https://github.com/snowflake-labs/snowcli"
-    url "https://files.pythonhosted.org/packages/76/dc/89bee53a044b5674fdce4f484683de4cb70443533ab145fa6aa2ffc8a7cb/snowflake_cli_labs-1.2.5.tar.gz"
-    sha256 "371d872e0a7e939fd5c6a56a36a8464fb08c3b9d27ed638352832c57331e8270"
+    url "https://files.pythonhosted.org/packages/03/69/b508427075311424de383944ecdd484918516e67081bda932779ef5360e5/snowflake_cli_labs-2.0.0.tar.gz"
+    sha256 "28ba342cf7b55a4d7aebb97bc1da4cd3ba7b05cd247dee11f52f80a1234ffdda"
 
     depends_on "python3"
 
@@ -14,7 +14,7 @@
       venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
         "-m", "pip", "install", "pip==22.3.1"
       venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip",
-        "install", "snowflake-cli-labs==1.2.5"
+        "install", "snowflake-cli-labs==2.0.0"
       bin.install_symlink "#{libexec}/bin/snow" => "snow"
     end
 
