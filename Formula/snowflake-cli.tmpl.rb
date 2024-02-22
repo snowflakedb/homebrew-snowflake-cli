dc2cc44dbc72560958659b981086518f2673fab7
Adam Stus <adam.stus@snowflake.com>
2024-02-22 10:54:25 +0100

    Added formula for snowflake-cli

--- Formula/snowflake-cli.tmpl.rb
+++ Formula/snowflake-cli.tmpl.rb
@@ -1,4 +1,4 @@
-  class Snowcli < Formula
+  class SnowflakeCli < Formula
     include Language::Python::Virtualenv
     desc "A CLI for Snowflake development"
     homepage "https://github.com/snowflake-labs/snowflake-cli"
@@ -9,11 +9,12 @@
 
     def install
       ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
-      venv = virtualenv_create(libexec, "python3", system_site_packages: false)
+      #without_pip=false because of https://github.com/Homebrew/brew/pull/15792
+      venv = virtualenv_create(libexec, "python3", system_site_packages: false, without_pip: false)
       venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
-        "-m", "ensurepip", "--upgrade"
-      venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
-        "-m", "pip", "install", "snowflake-cli-labs=={{ version }}"
+        "-m", "pip", "install", "pip==22.3.1"
+      venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip",
+        "install", "snowflake-cli-labs=={{ version }}"
       bin.install_symlink "#{libexec}/bin/snow" => "snow"
     end
 
