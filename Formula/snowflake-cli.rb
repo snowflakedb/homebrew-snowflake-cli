e3b5fc2b0978bd9a19fd45b6de1340862ad4ed41
Adam Stus <adam.stus@snowflake.com>
2024-02-22 13:36:40 +0100

    Virtualenv create fix

--- Formula/snowflake-cli.rb
+++ Formula/snowflake-cli.rb
@@ -9,8 +9,7 @@
 
     def install
       ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
-      #without_pip=false because of https://github.com/Homebrew/brew/pull/15792
-      venv = virtualenv_create(libexec, "python3", system_site_packages: false, without_pip: false)
+      venv = virtualenv_create(libexec, "python3", system_site_packages: true)
       venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
         "-m", "pip", "install", "pip==22.3.1"
       venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip",
