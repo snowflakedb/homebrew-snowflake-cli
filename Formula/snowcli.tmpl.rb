59e7caf7887cd0b03868c3dbee70ae55e069a090
Adam Stus <adam.stus@snowflake.com>
2024-02-23 14:49:48 +0100

    Python 3.12 fix

--- Formula/snowcli.tmpl.rb
+++ Formula/snowcli.tmpl.rb
@@ -9,12 +9,11 @@
 
     def install
       ENV["CARGO_NET_GIT_FETCH_WITH_CLI"] = "true"
-      #without_pip=false because of https://github.com/Homebrew/brew/pull/15792
-      venv = virtualenv_create(libexec, "python3", system_site_packages: false, without_pip: false)
+      venv = virtualenv_create(libexec, "python3", system_site_packages: false)
       venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
-        "-m", "pip", "install", "pip==22.3.1"
-      venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/pip",
-        "install", "snowflake-cli-labs=={{ version }}"
+        "-m", "ensurepip", "--upgrade"
+      venv.instance_variable_get(:@formula).system venv.instance_variable_get(:@venv_root)/"bin/python",
+        "-m", "pip", "install", "snowflake-cli-labs=={{ version }}"
       bin.install_symlink "#{libexec}/bin/snow" => "snow"
     end
 
