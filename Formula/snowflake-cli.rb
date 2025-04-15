ff50ea1a01f876465cbd5e878c1e889cc5d87ffd
Jan Sikorski <jan.sikorski@snowflake.com>
2025-04-15 06:41:59 +0200

    Update casks

--- Formula/snowflake-cli.rb
+++ Formula/snowflake-cli.rb
@@ -8,20 +8,20 @@ class SnowflakeCli < Formula
   if os == "darwin"
     if Hardware::CPU.intel?
       arch = "x86_64"
-      sha = "0dc0573d0964708ddbc72a8f8c419c16283a86d6f8e1779d1e5e40cbb723d4fe"
+      sha "0dc0573d0964708ddbc72a8f8c419c16283a86d6f8e1779d1e5e40cbb723d4fe"
     else
       arch = "arm64"
-      sha = "571062aa89f261193aa711468133de655d3346a2f6bf693b74f35fea888c4e51"
+      sha "571062aa89f261193aa711468133de655d3346a2f6bf693b74f35fea888c4e51"
     end
     url "https://sfc-repo.snowflakecomputing.com/snowflake-cli/darwin_#{arch}/#{version}/snowflake-cli-#{version}-#{os}-#{arch}.pkg"
 
   elsif os == "linux"
     if Hardware::CPU.intel?
       arch = "x86_64"
-      sha = "38d39b391d759ec27fdeabe2da902be0367b642632bf59ce1181ec15966a9dd5"
+      sha "38d39b391d759ec27fdeabe2da902be0367b642632bf59ce1181ec15966a9dd5"
     else
       arch = "aaarch64"
-      sha = "f42bef605184f19facff945848f80e9864469663da458d84ee0385da2ce67864"
+      sha "f42bef605184f19facff945848f80e9864469663da458d84ee0385da2ce67864"
     end
     ext = if which("dpkg")
             "deb"
@@ -37,8 +37,3 @@ class SnowflakeCli < Formula
     puts "download:  #{cached_download}"
     bin.install cached_download
   end
-
-  test do
-    system "#{bin}/snow", "--version"
-  end
-end
