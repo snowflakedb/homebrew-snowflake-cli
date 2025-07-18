7693312f23a31f6032cd2d346bd8675faeea7170
Jakub Wilkowski <jakub.wilkowski@snowflake.com>
2025-07-18 10:45:41 +0200

    Bump version to v3.10.0 (#60)
    * fix: fix version comparison
    
    * Update formula to v3.10.0

--- Casks/snowcli.rb
+++ Casks/snowcli.rb
@@ -2,14 +2,14 @@ cask "snowcli" do
   name "Snowflake CLI"
   desc "A CLI for Snowflake development"
   homepage "https://github.com/snowflakedb/snowflake-cli"
-  version "3.9.1"
+  version "3.10.0"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
   if Hardware::CPU.intel?
-    sha256 "dd860f0cf9b77e44367ae153fb9419a775b2635c32ac22bcc5ae6d7bb922111d"
+    sha256 "14201b76aebbcd17138ab971f8a6b1041e4ca9e4b61179411dd107f8cf81a0a7"
   else
-    sha256 "28a51351643faa8f9ece17e37980d57f38f2037fa06ad4a5f59c1083e7fde76e"
+    sha256 "bac48b0adae4e0f635dd975f2d3a3427c50e5acb43a7bccf037dfd933209c3a7"
   end
 
   url "https://sfc-repo.snowflakecomputing.com/snowflake-cli/darwin_#{arch}/#{version}/snowflake-cli-#{version}-darwin-#{arch}.pkg"
