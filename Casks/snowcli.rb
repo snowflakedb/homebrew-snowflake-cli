4871ffa015c606590ddba5a91f6cc992b2da9b8e
Jakub Wilkowski <jakub.wilkowski@snowflake.com>
2026-08-10 10:25:16 +0200

    chore: Update formula to v3.24.1

--- Casks/snowcli.rb
+++ Casks/snowcli.rb
@@ -2,14 +2,14 @@ cask "snowcli" do
   name "Snowflake CLI"
   desc "A CLI for Snowflake development"
   homepage "https://github.com/snowflakedb/snowflake-cli"
-  version "3.24.0"
+  version "3.24.1"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
   if Hardware::CPU.intel?
-    sha256 "64d81b71791398676b576a1f4f8a2c66e807aeda696e1def5c52d1e392133f0b"
+    sha256 "bf66af153bd327fe4ed2099d079a2f3bc20d2a16a02150c2f758efe0633e52a4"
   else
-    sha256 "5d6c78ae9f100dd5336ee70cdaa30992840f0834e298e76df188055b7cec2264"
+    sha256 "9c56530a50a9dc639d834d3c4b8e5a97c2bd3f213355ed45b3435ce7e8465ff7"
   end
 
   url "https://sfc-repo.snowflakecomputing.com/snowflake-cli/darwin_#{arch}/#{version}/snowflake-cli-#{version}-darwin-#{arch}.pkg"
