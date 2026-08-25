63d32bd148d26c03d5c867e543949859608efa28
Jakub Wilkowski <jakub.wilkowski@snowflake.com>
2026-08-25 09:09:11 +0200

    feat: bump 3.25.0 (#72)

--- Casks/snowflake-cli.rb
+++ Casks/snowflake-cli.rb
@@ -2,14 +2,14 @@ cask "snowflake-cli" do
   name "Snowflake CLI"
   desc "A CLI for Snowflake development"
   homepage "https://github.com/snowflakedb/snowflake-cli"
-  version "3.24.1"
+  version "3.25.0"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
 
   if Hardware::CPU.intel?
-    sha256 "bf66af153bd327fe4ed2099d079a2f3bc20d2a16a02150c2f758efe0633e52a4"
+    sha256 "7466d45f19af90129ac3017225b14c4fa06e3a8595f7c5f4063d02969835143d"
   else
-    sha256 "9c56530a50a9dc639d834d3c4b8e5a97c2bd3f213355ed45b3435ce7e8465ff7"
+    sha256 "e42929a91d1d9e7fed83b8788645c6a196bcb39e1bf9bebce0d4f72f9994f1f3"
   end
 
   url "https://sfc-repo.snowflakecomputing.com/snowflake-cli/darwin_#{arch}/#{version}/snowflake-cli-#{version}-darwin-#{arch}.pkg"
